adb35f375f54589d43961e36fce51fa89b560304
Marcus Little <mcolelittle@gmail.com>
2022-01-23 12:50:57 -0500

    add gatsby-image plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,7 @@
 module.exports = {
-    siteMetadata: {
-      title: `sweet gatsby site`,
-        siteUrl: `https://www.yourdomain.tld`,
-    },
-    plugins: [
-
-    ]
-}
+	siteMetadata: {
+		title: `sweet gatsby site`,
+		siteUrl: `https://www.yourdomain.tld`,
+	},
+	plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
 	return (
 		<Layout pageTitle="Home Page">
 			<p>I'm making this by following the gatsby tutorial</p>
+			<StaticImage src="../images/sad_knicks_pepe.jpeg" alt="sad knicks pepe" />
 		</Layout>
 	);
 };
