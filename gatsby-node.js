b43ad8ec4f7bb15436a327a227b7f8704a357499
Marcus Little <marcuslittle@Marcuss-MacBook-Pro.local>
2022-03-14 22:31:04 -0400

    change redirect

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,7 +2,7 @@ exports.createPages = async ({ graphql, actions }) => {
 	const { createRedirect } = actions;
 		
 	createRedirect({
-    fromPath: `/blog/*`,
+    fromPath: `/*`,
     toPath: `/test/*`,
   });
 }
