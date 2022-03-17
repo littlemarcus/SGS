8a4e6fb831fe87ef84c2e61b274d83e66ee82d7c
Marcus Little <marcuslittle@Marcuss-MacBook-Pro.local>
2022-03-17 00:33:10 -0400

    refactored redirects to static file to test

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,16 +1,11 @@
+const redirects = require("./redirects.json");
 exports.createPages = async ({ graphql, actions }) => {
-	const { createRedirect } = actions;
-		
-	createRedirect({
-    fromPath: `/*`,
-    toPath: `/test/*`,
-    conditions: {
-      country: `mx`
-    }
-  });
-
-  createRedirect({
-    fromPath: `/*`,
-    toPath: `/test/*`,
-  });
+  const { createRedirect } = actions;
+	
+  redirects.forEach(redirect => 
+    createRedirect({
+      fromPath: redirect.fromPath,
+      toPath: redirect.toPath,
+    })
+  );
 }
