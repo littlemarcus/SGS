4354833cd1e8a325e3467ff9765fb22064d2d534
Marcus Little <marcuslittle@Marcuss-MacBook-Pro.local>
2022-04-20 14:40:47 -0400

    add ssr to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,33 @@
 import * as React from "react";
 import Layout from "../components/layout";
 
-const AboutPage = () => {
+const AboutPage = ({serverData}) => {
 	return (
 		<Layout pageTitle="About Me">
 			<p>
 				Hi there! I'm the proud creator of this site, which I built with Gatsby.
 			</p>
+			<img alt="Happy dog" src={serverData.message} />
 		</Layout>
 	);
 };
 export default AboutPage;
+
+export async function getServerData() {
+	try {
+		const res = await fetch(`https://dog.ceo/api/breeds/image/random`)
+		if (!res.ok) {
+		  throw new Error(`Response failed`)
+		}
+		return {
+		  props: await res.json(),
+		}
+	  } catch (error) {
+		return {
+		  status: 500,
+		  headers: {},
+		  props: {}
+		}
+	  }
+
+}
