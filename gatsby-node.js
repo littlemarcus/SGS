33ab3331ed90f7b2e0c7a3229c404e0b37dd7602
“Marcus <mcolelittle@gmail.com>
2022-06-20 21:38:12 -0400

    adjust redirect:

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,7 +2,7 @@ exports.createPages = async ({ graphql, actions }) => {
 	const { createRedirect } = actions;
 		
     createRedirect({
-        fromPath: `/first-post`,
+        fromPath: `/blog/*`,
         redirectInBrowser: true,
         toPath: `https://www.eql.xyz/en-US/cookie-policy/`,
         statusCode: 200,
