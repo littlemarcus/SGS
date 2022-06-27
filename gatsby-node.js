5696d5bd2f03dd3fa28fce9f4fac6f6c71cca7ce
“Marcus <mcolelittle@gmail.com>
2022-06-27 16:11:03 -0400

    change rewrite

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,7 +3,7 @@ exports.createPages = async ({ graphql, actions }) => {
 		
     createRedirect({
         fromPath: `/blog/*`,
-        toPath: "https://contentfulredirecttest.gatsbyjs.io/",
+        toPath: "https://contentfulredirecttest.gatsbyjs.io",
         statusCode: 200,
       });
 }
